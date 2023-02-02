bc8bc7511b6cf0e1448236023186d2dc74aa7e84
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2023-02-02 16:39:44 +0530

    updated Code

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,20 +5,11 @@ namespace DataGridMAUI;
 
 public partial class MainPage : ContentPage
 {
-    public SfDataGrid dataGrid;
     EmployeeViewModel viewModel;
     public MainPage()
 	{
 		InitializeComponent();
-        dataGrid = new SfDataGrid() { ColumnWidthMode = ColumnWidthMode.Auto };
-        viewModel = new EmployeeViewModel();
-        dataGrid.ItemsSource = viewModel.Employees;
-        dataGrid.AutoGenerateColumnsMode = AutoGenerateColumnsMode.None;
         dataGrid.CellRenderers.Add("Stepper", new StepperColumnRenderer());
-        var customColumn = new StepperColumn();
-        customColumn.MappingName = "OrderID";
-        dataGrid.Columns.Add(customColumn);
-        this.Content = dataGrid;
     }	
 }
 
