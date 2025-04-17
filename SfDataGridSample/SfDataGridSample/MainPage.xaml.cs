5b617bd5144e5a2a09a58fc2cc13f4819ce605b7
SethupathyD <sethupathy.d@syncfusion.com>
2025-04-17 14:29:25 +0530

    Code updated

---
+++ SfDataGridSample/SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        EmployeeViewModel viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            dataGrid.CellRenderers.Add("Stepper", new StepperColumnRenderer());
+        }
+    }
+}
